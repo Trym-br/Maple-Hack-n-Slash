647ad4e0e147a7bce93a6ae0b81a95efd8e662d3
Trym-br <trym.brandal.arhaug@gmail.com>
2024-09-27 10:39:36 +0200

    Made Player Prefab and basic movement

--- Maple Hack n Slash/Assets/Scripts/Player/InputActions.cs
+++ Maple Hack n Slash/Assets/Scripts/Player/InputActions.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
 
-public class Input : MonoBehaviour
+public class InputActions : MonoBehaviour
 {
     private InputSystem_Actions _input;
 
-    public Vector2 MoveDirection;
-    public bool attack;
-    public bool secondaryAction;
+    // public Vector2 MoveDirection;
+    public float Horizontal;
+    public float Vertical;
+    public bool Attack;
+    public bool SecondaryAction;
 
     private void Update()
     {
-        MoveDirection = _input.Player.Move.ReadValue<Vector2>();
-        attack = _input.Player.Attack.WasPressedThisFrame();
-        secondaryAction = _input.Player.Attack.WasPressedThisFrame();
+        // MoveDirection = _input.Player.Move.ReadValue<Vector2>();
+        Attack = _input.Player.Attack.WasPressedThisFrame();
+        SecondaryAction = _input.Player.Attack.WasPressedThisFrame();
+        Horizontal = _input.Player.Move.ReadValue<Vector2>().x;
+        Vertical = _input.Player.Move.ReadValue<Vector2>().y;
     }
 
     private void Awake() { _input = new InputSystem_Actions(); }
--- Maple Hack n Slash/Assets/Scripts/Player/PlayerController.cs
+++ Maple Hack n Slash/Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 5f;
+    public float jumpSpeed = 4f;
+
+    private InputActions _input;
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
     {
-        
+        _input = GetComponent<InputActions>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.linearVelocityX = _input.Horizontal * moveSpeed;
+        _rigidbody2D.linearVelocityY = _input.Vertical * moveSpeed;
     }
 }
+
