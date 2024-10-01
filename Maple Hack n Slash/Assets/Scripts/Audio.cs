a2f4fbf388ed990356f86baa0635f1790596a38b
Veroo24 <veronikagiske24@gmail.com>
2024-10-01 16:27:35 +0200

    Audio test

---
+++ Maple Hack n Slash/Assets/Scripts/Audio.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Audio : MonoBehaviour
+{
+    // Source for Audio and Music
+    private AudioSource _audioSource;
+    public AudioClip _clip1;
+    
+    private void Start()
+    {
+        // Get AudioSource Component
+        _audioSource = GetComponent<AudioSource>();
+        // Set AudioSource Clip
+        _audioSource.clip = _clip1;
+        // Play Music
+        _audioSource.Play();
+    }
+
+    private void Update()
+    {
+        _audioSource.PlayOneShot(_clip1);
+    } 
+}
